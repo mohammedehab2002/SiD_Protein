cb7c4d85567f0c85227e1de1273dc4b1bf66af5a
ehab02 <ehab02@login009.inband>
2025-12-20 14:53:37 -0500

    sloan cluster mini-fixes

--- run_sid_proteina.sh
+++ run_sid_proteina.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#SBATCH -p sched_mit_sloan_gpu_r8 --gres=gpu:a100:2 -t 24:00:00 --mem=256G
+#SBATCH -o length_distribution.out
+
+conda activate sid_protein_env
+
 # Run the code below in command window to set CUDA visible devices and run specific script
 # export CUDA_VISIBLE_DEVICES=4,5,6,7
 # export NCCL_DEBUG=INFO
@@ -12,13 +17,13 @@
 # Add the option below to load a checkpoint:
 # Many options are optional, such as --data_stat, which will be computed inside the code if not provided
 # --resume 'protein_experiment/sid-train-runs/proteina_multistep/00000-Proteina_Uncond???/network-snapshot???.pkl'
-torchrun --standalone --nproc_per_node=4 sid_train.py \
+torchrun --standalone --nproc_per_node=2 sid_train.py \
 --alpha 1.0 \
 --t_init 30 \
 --t 400 \
 --tmax 0.98 \
 --batch 4096 \
---batch-gpu 2 \
+--batch-gpu 4 \
 --eval_batch 5 \
 --outdir 'protein_experiment/sid-train-runs/proteina_multistep' \
 --data 'pdb_raw/cath_label_mapping.pt' \
@@ -39,6 +44,7 @@ torchrun --standalone --nproc_per_node=4 sid_train.py \
 --config_path 'proteina/configs/experiment_config/' \
 --config_name 'inference_ucond_200m_notri' \
 --noise_scale 0.4 \
---nstep 10 \
+--nstep 1 \
 --min_n_res 50 \
 --max_n_res 256 \
+--resume 'protein_experiment/sid-train-runs/proteina_multistep/00034-uncond-proteina-glr5e-05-lr0.0001-initsigma2.5-gpus2-alpha1.0-batch4096-tmax0.98-fp16-nstep1/network-snapshot-1.000000-000577.pkl'
